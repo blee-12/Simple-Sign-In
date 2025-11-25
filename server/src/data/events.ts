603f06deb018106eda9b49e248d64059db3bcce7
Woodsii <beanmanwoods@gmail.com>
2025-11-25 18:21:56 -0500

    impliment user data functions

--- server/src/data/events.ts
+++ server/src/data/events.ts
@@ -1,5 +1,6 @@
 import { ObjectId, Collection } from "mongodb";
-import { events, Event } from "../config/mongoCollections.js";
+import { events, Event } from "../config/mongoCollections.ts";
+import { validateStrAsObjectId } from "../../../common/validation.ts";
 
 // example data function
 let exportedMethods = {
@@ -8,6 +9,19 @@ let exportedMethods = {
     const eventList = await eventCollection.find({}).toArray();
     return eventList;
   },
+
+  async getEventByID(id: string) {
+      id = validateStrAsObjectId(id);
+  
+      const eventCollection = await events();
+      const event = await eventCollection.findOne({ _id: new ObjectId(id) });
+  
+      if (!event) {
+        throw new Error(`Could not find event with id: ${id}`);
+      }
+  
+      return event;
+    },
 };
 
 export default exportedMethods;
--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -1,6 +1,17 @@
 import { ObjectId, Collection } from "mongodb";
-import { users, User } from "../config/mongoCollections.js";
-import { validateEmail, validateFirstName } from "../../../common/validation.js"; 
+import { users, User } from "../config/mongoCollections.ts";
+import { validateEmail, validateFirstName, validateLastName, validatePassword, validateStrAsObjectId } from "../../../common/validation.ts"; 
+
+// helper function, checks if the email is in use:
+async function emailInUse(email: string): Promise<Boolean> { 
+  // asuming that its already a valid email
+
+  const userCollection = await users();
+  const inUse = await userCollection.findOne({email: email});
+
+  if (inUse) throw new Error("Email is already in use!");
+  return false;
+}
 
 // example data function
 let exportedMethods = {
@@ -11,13 +22,7 @@ let exportedMethods = {
   },
 
   async getUserByID(id: string) {
-    if (!id || typeof id !== "string" || id.trim().length === 0) {
-      throw new Error("Id must be a non-empty string");
-    }
-
-    if (!ObjectId.isValid(id)) {
-      throw new Error("Invalid ObjectId format");
-    }
+    id = validateStrAsObjectId(id);
 
     const userCollection = await users();
     const user = await userCollection.findOne({ _id: new ObjectId(id) });
@@ -30,7 +35,7 @@ let exportedMethods = {
   },
 
   async getUserByEmail(email: string) {
-    validateEmail(email);
+    email = validateEmail(email);
 
     const userCollection = await users();
     const user = await userCollection.findOne({ email: email});
@@ -43,8 +48,7 @@ let exportedMethods = {
   },
 
   async deleteUser(id: string) {
-
-    // validate ID
+    id = validateStrAsObjectId(id)
 
     const userCollection = await users();
     const user = await userCollection.findOneAndDelete({_id: new ObjectId(id)});
@@ -56,34 +60,68 @@ let exportedMethods = {
     return user;
   },
 
-  async editUser(id: string, email: string | null, first_name: string | null, last_name: string | null, created_events: string | null, attended_events: string | null) {
+  async editUser(email: string, first_name: string | null, last_name: string | null, created_events: string[] | null, attended_events: string[] | null) {
     // first get the user
-    let user = await this.getUserByID(id);
+    let originalEmail = email;
+    let user = await this.getUserByEmail(email);
     let modified = false;
 
     // then we're just gonna update the user object:
     if (email) {
-      validateEmail(email);
+      email = validateEmail(email);
+      emailInUse(email)
       user.email = email;
       modified = true;
     }
 
     if (first_name) {
-      validateFirstName(first_name);
+      first_name = validateFirstName(first_name);
       user.first_name = first_name;
       modified = true;
     }
 
     if (last_name) {
-      validateFirstName(last_name);
+      last_name = validateLastName(last_name);
       user.last_name = last_name;
       modified = true;
     }
 
-    if (created_events) {
-      // validate create_events
+    if (!modified){
+      throw new Error("Must change provide more than just an email argument!");
     }
 
+    const userCollection = await users();
+    const ret = await userCollection.findOneAndReplace({email: originalEmail}, user);
+
+    if (!ret) {
+      throw new Error(`could not edit user with email ${email}!`);
+    }
+
+    return user;
+  },
+
+  async addUser(email: string, first_name: string, last_name: string, password: string) {
+    email = validateEmail(email);
+    emailInUse(email);
+
+    first_name = validateFirstName(first_name);
+    last_name = validateLastName(last_name);
+    password = validatePassword(password);
+
+    // and now create a user object
+    const user: User = { 
+      email,
+      first_name,
+      last_name,
+      password
+    }
+
+    const userCollection = await users();
+    const added = await userCollection.insertOne(user);
+
+    if (!added) throw new Error(`Could not add user ${email}!`);
+
+    return user;
   }
 };
 
