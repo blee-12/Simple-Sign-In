cbf440ae3da4f22c3a02df95aaf5666f59c6c6aa
Woodsii <beanmanwoods@gmail.com>
2025-12-03 17:39:49 -0500

    impliment user data funcs

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -1,5 +1,6 @@
 import { ObjectId, Collection } from "mongodb";
-import { users, User } from "../config/mongoCollections.ts";
+import { users } from "../config/mongoCollections.ts";
+import type { User } from "../config/mongoCollections.ts";
 import { validateEmail, validateFirstName, validateLastName, validatePassword, validateStrAsObjectId } from "../../../common/validation.ts"; 
 
 // helper function, checks if the email is in use:
@@ -60,20 +61,12 @@ let exportedMethods = {
     return user;
   },
 
-  async editUser(email: string, first_name: string | null, last_name: string | null, created_events: string[] | null, attended_events: string[] | null) {
+  async editUser(email: string, first_name: string | null, last_name: string | null) {
     // first get the user
-    let originalEmail = email;
     let user = await this.getUserByEmail(email);
     let modified = false;
 
     // then we're just gonna update the user object:
-    if (email) {
-      email = validateEmail(email);
-      emailInUse(email)
-      user.email = email;
-      modified = true;
-    }
-
     if (first_name) {
       first_name = validateFirstName(first_name);
       user.first_name = first_name;
@@ -91,7 +84,14 @@ let exportedMethods = {
     }
 
     const userCollection = await users();
-    const ret = await userCollection.findOneAndReplace({email: originalEmail}, user);
+    const ret = await userCollection.findOneAndReplace({ email: email }, 
+      { $set: 
+        { 
+          first_name: user.first_name, 
+          last_name: user.last_name 
+        }
+      }
+    );
 
     if (!ret) {
       throw new Error(`could not edit user with email ${email}!`);
@@ -102,7 +102,7 @@ let exportedMethods = {
 
   async addUser(email: string, first_name: string, last_name: string, password: string) {
     email = validateEmail(email);
-    emailInUse(email);
+    await emailInUse(email);
 
     first_name = validateFirstName(first_name);
     last_name = validateLastName(last_name);
