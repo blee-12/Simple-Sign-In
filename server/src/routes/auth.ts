974381886fc30c63af12983309ad7b87ca70568a
Someone109 <praneeth.vanguru@gmail.com>
2025-12-12 00:38:31 -0500

    Added Login/SignIn Pages

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,60 +2,65 @@
 // POST /signin
 // GET /signout
 
-import bcrypt from 'bcryptjs';
-import { Request, Response, Router } from 'express';
-import { validateFirstName, validateLastName, validateEmail, validatePassword } from '../../../common/validation';
-import { User } from '../config/mongoCollections';
+import bcrypt from "bcryptjs";
+import { Request, Response, Router } from "express";
+import {
+  validateFirstName,
+  validateLastName,
+  validateEmail,
+  validatePassword,
+} from "../../../common/validation";
+import { User } from "../config/mongoCollections";
 
 const router = Router();
 
 router.post("/signup", async (req: Request, res: Response) => {
-    let first_name, last_name, email, password;
-    try {
-        if (!req.body) throw new Error("No body provided");
-        first_name = validateFirstName(req.body.first_name);
-        last_name = validateLastName(req.body.last_name);
-        email = validateEmail(req.body.email);
-        password = validatePassword(req.body.password);
-    } catch (err: any) {
-        return res.status(400).send({error: `${err.message}`});
-    }
-    // TODO: validation passed, check if in DB and store if not
-    password = await bcrypt.hash(password, 10);
-    // save to session
-    req.session.first_name = first_name;
-    req.session.last_name = last_name;
-    req.session.email = email;
-    res.send({message: "registered & signed in"});
+  let first_name, last_name, email, password;
+  try {
+    if (!req.body) throw new Error("No body provided");
+    first_name = validateFirstName(req.body.first_name);
+    last_name = validateLastName(req.body.last_name);
+    email = validateEmail(req.body.email);
+    password = validatePassword(req.body.password);
+  } catch (err: any) {
+    return res.status(400).send({ error: `${err.message}` });
+  }
+  // TODO: validation passed, check if in DB and store if not
+  password = await bcrypt.hash(password, 10);
+  // save to session
+  req.session.first_name = first_name;
+  req.session.last_name = last_name;
+  req.session.email = email;
+  res.send({ message: "registered & signed in" });
 });
 
 router.post("/signin", async (req: Request, res: Response) => {
-    let email, password;
-    try {
-        if (!req.body) throw new Error("No body provided");
-        email = validateEmail(req.body.email);
-        password = req.body.password;
-    } catch (err: any) {
-        return res.status(400).send({error: `${err.message}`});
-    }
-    // TODO: check against DB
-    // ...
-    let user: User; // retrieved from DB
-    // save to session
-    // req.session.first_name = user.first_name; // TODO: uncomment when user is actually fetched
-    // req.session.last_name = user.last_name; // TODO: uncomment when user is actually fetched
-    req.session.email = email;
-    res.send({status: "signed in"});
+  let email, password;
+  try {
+    if (!req.body) throw new Error("No body provided");
+    email = validateEmail(req.body.email);
+    password = req.body.password;
+  } catch (err: any) {
+    return res.status(400).send({ error: `${err.message}` });
+  }
+  // TODO: check against DB
+  // ...
+  let user: User; // retrieved from DB
+  // save to session
+  // req.session.first_name = user.first_name; // TODO: uncomment when user is actually fetched
+  // req.session.last_name = user.last_name; // TODO: uncomment when user is actually fetched
+  req.session.email = email;
+  res.send({ status: "signed in" });
 });
 
 router.get("/signout", async (req: Request, res: Response) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.error(`Error destroying session: ${err}`);
-            return res.status(500).send({error: "Error signing out"});
-        }
-        return res.send({status: "signed out"});
-    });
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(`Error destroying session: ${err}`);
+      return res.status(500).send({ error: "Error signing out" });
+    }
+    return res.send({ status: "signed out" });
+  });
 });
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import { Request, Response } from 'express';
-import configRoutes from './routes/index'
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+import express from "express";
+import { Request, Response } from "express";
+import configRoutes from "./routes/index";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import session from "express-session";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
@@ -13,23 +13,23 @@ app.use(cors());
 
 // Session
 const sessionMiddleware = session({
-    name: "AuthenticationState",
-    secret: "some secret string!",
-    resave: false,
-    saveUninitialized: false,
+  name: "AuthenticationState",
+  secret: "some secret string!",
+  resave: false,
+  saveUninitialized: false,
 });
 app.use(sessionMiddleware);
 
 declare module "express-session" {
-    interface SessionData {
-        first_name: string,
-        last_name: string,
-        email: string,
-        password: string
-    }
+  interface SessionData {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+  }
 }
 
-configRoutes(app)
+configRoutes(app);
 
 // socket.io
 const io = new Server(httpServer);
@@ -37,30 +37,29 @@ io.engine.use(sessionMiddleware);
 io.engine.use(cors());
 
 io.on("connection", (socket) => {
-    console.log("socket connection received");
-    // @ts-ignore
-    const session = socket.request.session;
-    if (session.email) {
-        console.log(`session ${session.email}`);
-    }
-    else {
-        console.log("no session");
-    }
-    // TODO: remove when we actually do something with the socket
-    socket.disconnect();
+  console.log("socket connection received");
+  // @ts-ignore
+  const session = socket.request.session;
+  if (session.email) {
+    console.log(`session ${session.email}`);
+  } else {
+    console.log("no session");
+  }
+  // TODO: remove when we actually do something with the socket
+  socket.disconnect();
 });
 
 // fallback error handler
 app.use(async (err: any, req: Request, res: Response, next: any) => {
-    if (err.statusCode < 500)
-        return res.status(err.statusCode).send({error: err.message});
-    console.error("Unhandled server error:");
-    console.error(err);
-    return res.status(500).send({error: "Internal server error"});
+  if (err.statusCode < 500)
+    return res.status(err.statusCode).send({ error: err.message });
+  console.error("Unhandled server error:");
+  console.error(err);
+  return res.status(500).send({ error: "Internal server error" });
 });
 
-httpServer.listen(3000, () => {
-    console.log("Express server has started!");
-})
+httpServer.listen(3001, () => {
+  console.log("Express server has started!");
+});
 
 export const simpleSignInServer = app;
