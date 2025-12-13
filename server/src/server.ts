520d7f8ac5088eae605880f9f6ca8c431a3d6a37
Woodsii <beanmanwoods@gmail.com>
2025-12-13 17:54:09 -0500

    scaffold websocket server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,15 +7,31 @@ import session from "express-session";
 import cors from "cors";
 import { ObjectId } from 'mongodb';
 import { CLIENT_URL } from "./config/staticAssets";
+import { ClientToServerEvents, ServerToClientEvents } from "../../common/socketTypes.ts";
 
 // types
-
-export interface EventState {
+interface EventState {
   id: string;
   name: string;
-  currentCode: string;
-  intervalId: NodeJS.Timeout;
+  currentCode: string; // deal with code bs later tbh.
+  intervalId: NodeJS.Timeout; 
 }
+
+declare module "express-session" {
+  interface SessionData {
+    _id: ObjectId;
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+  }
+}
+
+declare module "http" {                                       
+   interface IncomingMessage {                             
+        session: session.SessionData;                        
+    }                                                        
+  }
 
 const app = express();
 const httpServer = createServer(app);
@@ -36,32 +52,61 @@ const sessionMiddleware = session({
 });
 app.use(sessionMiddleware);
 
-declare module "express-session" {
-  interface SessionData {
-    _id: ObjectId;
-    first_name: string;
-    last_name: string;
-    email: string;
-    password: string;
-  }
-}
-
 configRoutes(app);
 
-// socket.io
-const io = new Server(httpServer);
+
+/* ~~ SOCKET.IO SECTION ~~ */
+
+const io = new Server <ClientToServerEvents, ServerToClientEvents> (httpServer, {
+    cors: {
+        origin: "http://localhost:5173", // need to replace with front end url eventually.
+        credentials: true 
+    }
+});
 io.engine.use(sessionMiddleware);
 io.engine.use(cors());
 
+// to store active events
+// this will need syncronization
+const activeEvents = new Map<string, EventState>();
+
 io.on("connection", (socket) => {
   console.log("socket connection received");
-  // @ts-ignore
-  const session = socket.request.session;
-  if (session.email) {
-    console.log(`session ${session.email}`);
-  } else {
-    console.log("no session");
+  const req = socket.request;
+  
+  // make sure that the user is signed in.
+  if (!req.session || !req.session._id) {
+    console.log("Unauthenticated, disconnecting.")
   }
+
+  // handle check in
+  socket.on("check_in", (eventId, code, email) => {
+    // TODO: check if the map has that event active.
+    // verify the code matches.
+
+    const roomName = `${eventId}_chat`;
+    socket.join(roomName);
+
+    socket.emit("success_join");
+
+    io.to('${eventId}_creator').emit("student_checked_in", req.session.email);
+
+    // TODO: if the student isn't registered for the event, then handle that.
+
+    console.log(`${req.session.email} joined room: ${roomName}`);
+  });  
+
+  // handle messages
+  socket.on("send_message", (eventId, message) => {
+    const roomName = `${eventId}_chat`
+
+    if (socket.rooms.has(roomName)) {
+        io.to(roomName).emit("chat_message", req.session.email, message); 
+    } else {
+        socket.emit("error", "You must join the event before sending a message!");
+    }
+  });
+
   // TODO: remove when we actually do something with the socket
   socket.disconnect();
 });
