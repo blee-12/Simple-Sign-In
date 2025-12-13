8144777862400ef8e3950c8744a0d0ee73d8bd1e
Brendan Lee <blee12@stevens.edu>
2025-12-13 16:21:14 -0500

    Error Fixes + Middleware

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -1,16 +1,10 @@
 // GET /
-// GET /dashboard
 
 import { Request, Response, Router } from 'express';
 
 const router = Router()
 
-// GET users dashboard. Can separate to another file if more routes needed
-router.get('/dashboard', (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Dashboard data fetched' });
-});
-
-// GET root placeholder. May remove if no data needed from this endpoint
+// Placeholder
 router.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello World' });
 });
