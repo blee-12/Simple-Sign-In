f5adbcf8ca2268a99d03a9aa0da7282b1313852d
Brendan Lee <blee12@stevens.edu>
2025-12-14 00:12:11 -0500

    Nav Bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,13 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { SignUp } from "./components/auth/SignUp";
 import { LogIn } from "./components/auth/LogIn";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+    {/* TODO replace false with session auth context */}
+    <Navbar isAuthenticated={false} />
       <Routes>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/login" element={<LogIn />}></Route>
