6dcb915122913822318ff45a91804974ae37ffee
Woodsii <beanmanwoods@gmail.com>
2025-12-15 21:41:36 -0500

    update routing to include event page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { SignUp } from "./components/auth/SignUp";
 import { LogIn } from "./components/auth/LogIn";
+import { EventPage } from "./components/EventPage"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/event/:id" element={<EventPage />}></Route>
       </Routes>
     </>
   );
