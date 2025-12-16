43edc95ac684a02af0feb00dce8825c817b61893
Brendan Lee <blee12@stevens.edu>
2025-12-16 15:25:20 -0500

    profile password update

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -2,22 +2,36 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { asyncRoute, requireAuth } from './utils';
 import * as val from '../../../common/validation';
 import users from '../data/users'
+import { BadInputError } from '../../../common/errors';
 const router = Router()
 
 router.get('/', requireAuth, asyncRoute ( 
     async (req: Request, res: Response, next: NextFunction) => {
         let id = req.session._id || ""
-        id = val.validateStrAsObjectId(id)
         const user = await users.getUserByID(id)
-        res.status(200).json({ data: user });
+        const response = {
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name
+        }
+        res.status(200).json({ data: response });
     }
 ));
 router.put('/', requireAuth, asyncRoute (
     async (req: Request, res: Response, next: NextFunction) => {
-        let id = req.session._id || ""
-        id = val.validateStrAsObjectId(id)
-        const user = await users.getUserByID(id)
-        res.status(200).json({ data: user });
+        let email = req.session.email || ""
+        let { first_name, last_name, password } = req.body  // user inputs. Must provide at least 1
+
+        if (!first_name.trim() && !last_name.trim() && !password().trim())
+            throw new BadInputError("No updated fields provided")
+        
+        const user = await users.editUser(email, first_name, last_name, password)
+        const response = {
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name
+        }
+        res.status(200).json({ data: response });
     }
 ));
 router.delete('/', requireAuth, asyncRoute (
@@ -34,16 +48,13 @@ router.delete('/', requireAuth, asyncRoute (
         })
         
         const user = await users.deleteUser(id)
-        res.status(200).json({ data: user })
+        const response = {
+            email: user.email,
+            first_name: user.first_name,
+            last_name: user.last_name
+        }
+        res.status(200).json({ data: response })
     }
 ));
-
-
-//NOTE add /:id routes if need to access users != self
-
-// Get all route also questionable. When should one logged in user have access to all others..?
-// router.get('/users', requireAuth, (req: Request, res: Response, next: NextFunction) => {
-//     res.status(200).json({ message: `Users fetched` });
-// });
 
 export default router
